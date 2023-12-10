62fc8a8b3c0013c1c4b1559596cfcfd207532a20
Aji Muhammad Sofwan <ajimsofwan@gmail.com>
2023-12-10 14:08:50 +0700

    push

---
+++ src/components/Card.vue
@@ -0,0 +1,7 @@
+
+<template>
+  <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    xxx
+    <slot></slot>
+  </div>
+</template>
---
+++ src/components/JobList.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Job from '../types/Job';
+import Card from './Card.vue';
+
+defineProps<{
+  jobs: Job[]
+}>()
+
+</script>
+
+<template>
+  <div v-for="job in jobs" :key="job.id">
+    <Card>
+      {{ job.title }}
+      {{ job.location }}
+      {{ job.salary }}
+    </Card>
+  </div>
+</template>
